5bcb53dc51fecd7c2f5f083beaedcfb09a387c26
Shreeda Bhat <shreedabhatt5@gmail.com>
2026-09-20 04:06:56 +0530

    Delete Mathematics_and_Algorithms/Pascal_s Triangle alt.py

--- Mathematics_and_Algorithms/Pascal_s Triangle alt.py
+++
@@ -1,9 +0,0 @@
-from math import factorial
-k=int(input("Enter The Number of Rows in Pascal's Triangle:"))
-for i in range(k+1):
-    for j in range(i+1):
-        a=factorial(i)
-        b=factorial(j)
-        c=factorial(i-j)
-        print(int(a/(b*c)),end=' ')
-    print()
